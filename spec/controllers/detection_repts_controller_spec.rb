b3959c408b853e6f5ae2afed851c36a75aa50ba2
fishaction <micrasoft.bf4@gmail.com>
2022-08-26 02:09:21 +0900

    DetectionReptモデルの作成，それに伴うDeviceのリレーションに関する記述．等

---
+++ spec/controllers/detection_repts_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DetectionReptsController, type: :controller do
+
+end
---
+++ spec/factories/detection_repts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :detection_rept do
+    x_coord { "9.99" }
+    y_coord { "9.99" }
+  end
+end
